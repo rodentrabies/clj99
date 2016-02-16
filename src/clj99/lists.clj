6cf3ab4f1454b8c0e2d8d1eed46a8457158a1c05
Yuri Zhykin <yuri.zhykin@gmail.com>
2016-02-16 23:19:14 +0200

    +4

--- src/clj99/lists.clj
+++ src/clj99/lists.clj
@@ -48,3 +48,55 @@
     (if (seq s)
       (recur (rest s) (inc ln))
       ln)))
+
+
+(defn mreverse
+  "P05 (*) Reverse a list."
+  [lst]
+  (loop [l lst, res nil]
+    (if (seq l)
+      (recur (rest l) (concat (list (first l)) res))
+      res)))
+
+
+(defn palindrome?
+  "P06 (*) Find out whether a list is a palindrome.
+   A palindrome can be read forward or backward; e.g. (x a m a x)."
+  [lst]
+  (every? identity (map = lst (reverse lst))))
+
+
+(defn mflatten
+  "P07 (**) Flatten a nested list structure.
+   Transform a list, possibly holding lists as elements into a `flat' list by
+   replacing each list with its elements (recursively).
+   Hint: Use the predefined functions list and append.
+   Example:
+     * (my-flatten '(a (b (c d) e)))
+     (A B C D E)
+  "
+  [tree]
+  (letfn [(mloop% [t res]
+            (if (seq t)
+              (if (seq? (first t))
+                (recur (rest t) (concat (mloop% (first t) nil) res))
+                (recur (rest t) (cons (first t) res)))
+              res))]
+    (reverse (mloop% tree nil))))
+
+
+(defn compress
+  "P08 (**) Eliminate consecutive duplicates of list elements.
+   If a list contains repeated elements they should be replaced with a single
+   copy of the element. The order of the elements should not be changed.
+   Example:
+     * (compress '(a a a a b c c a a d e e e e))
+     (A B C A D E)
+  "
+  [lst]
+  (loop [c (first lst), l lst, res (and c (list c))]
+    (if (seq l)
+      (if (= (first l) c)
+        (recur c (rest l) res)
+        (recur (first l) (rest l) (cons (first l) res)))
+      (reverse res))))
--- test/clj99/lists_test.clj
+++ test/clj99/lists_test.clj
@@ -17,3 +17,22 @@
 (deftest p04
   (testing "P04"
     (is (= (mlen '(a b c d e)) 5))))
+
+(deftest p05
+  (testing "P05"
+    (is (= (mreverse '(a b c)) '(c b a)))))
+
+(deftest p06
+  (testing "P05"
+    (is (= (palindrome? '(x a m a x)) true))
+    (is (= (palindrome? '()) true))
+    (is (= (palindrome? '(a b c)) false))))
+
+(deftest p07
+  (testing "P07"
+    (is (= (mflatten '(a (b (c d) e))) '(a b c d e)))))
+
+(deftest p08
+  (testing "P08"
+    (is (= (clj99.lists/compress '(a a a a b c c a a d e e e e))
+           '(a b c a d e)))))
